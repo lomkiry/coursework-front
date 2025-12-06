1b3feb86353aa5bd50a360540dfb08e71671ab5a
kirill <kirillrabotaxd@gmail.com>
2025-12-06 17:05:15 +0700

    add histoy and че то

--- coursework/src/app/app.routes.ts
+++ coursework/src/app/app.routes.ts
@@ -1,7 +1,15 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './dashboard/dashboard';
+import { Transactions } from './transactions/transactions-list';
+import { History } from './history/history';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
     { path: 'dashboard', component: Dashboard },
+
+    { path: 'transactions', component: Transactions},
+
+    { path: 'history', component: History},
+
+    { path: '**', redirectTo: 'dashboard' }
 ];
